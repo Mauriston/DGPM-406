fd79a2f407c03c4fc8f056473458757fc8bd7972
Mauriston <mauriston@oncoortopedia.com>
2025-11-24 02:12:28 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// SUBSTITUA 'nome-do-seu-repositorio' PELO NOME QUE ESTÁ NO GITHUB
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // Adicione esta linha abaixo:
+      base: '/nome-do-seu-repositorio/', 
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
